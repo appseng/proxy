0777b11cc38e30593b1a046c2811106ae9ab8e7e
Дмитрий <appseng@yandex.ru>
2021-04-21 18:23:29 +0300

    check if URL exists

--- Classes/Proxy.php
+++ Classes/Proxy.php
@@ -39,54 +39,62 @@ class Proxy {
         $page = isset($_GET[$this->params['proxyPageParam']]) ? $_GET[$this->params['proxyPageParam']] : '';
 
         $validHostnameRegex = "^https?://([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9/]))*";
+        
         if ($page !== '' && preg_match("~$validHostnameRegex~", $page) == 1) {
-            $this->URL = $this->util->getURL($page);
-            //echo $this->URL;
-            $this->fetchPage();
 
-            $this->pageHTML = preg_replace("~target=(\"|')?_blank(\"|')?~", '', $this->pageHTML);
-            
-            $util = $this->util;
-            //proxy img|script|link|input|meta
-            $this->pageHTML = preg_replace_callback("~<(img|script|link|input|meta)(\s[^>]*?\s*)(href|src|content)=['\"]?([^>'\"\s]*)(\.)?(jpeg|jpg|gif|png|svg|css|js|ico)([^>'\"\s]*)['\"\s]?([^>]*)>~im", function($m) use($util) {
-                $src = $m[4].$m[5].$m[6].$m[7];
-                $src = $util->replaceLink($src);
-                return "<{$m[1]}{$m[2]}{$m[3]}=\"{$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src\"{$m[8]}>";
-            }, $this->pageHTML);
+            $fileHeaders = @get_headers($page);
+            if(!$fileHeaders || $fileHeaders[0] == 'HTTP/1.1 404 Not Found') {
+                $this->echoDefault();
+            }
+            else {
+                $this->URL = $this->util->getURL($page);
+                //echo $this->URL;
+                $this->fetchPage();
 
-            //proxy style="... url() ..."
-            $this->pageHTML = preg_replace_callback("~<([^>]*)style=['\"]?([^>'\"]*)url\s*\(([^>'\"\s]*)\.(jpeg|jpg|gif|png|svg|ico)([^>'\"\s\)]*)\)([^>\"\']*)['\"]?([^>]*)>~im", function($m) use($util) {
-                $src = $m[3].'.'.$m[4].$m[5];
-                $src = $util->replaceLink($src);                
-                return "<{$m[1]}style=\"{$m[2]}url({$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src){$m[6]}\"{$m[7]}>";
-            }, $this->pageHTML);
+                $this->pageHTML = preg_replace("~target=(\"|')?_blank(\"|')?~", '', $this->pageHTML);
+                
+                $util = $this->util;
+                //proxy img|script|link|input|meta
+                $this->pageHTML = preg_replace_callback("~<(img|script|link|input|meta)(\s[^>]*?\s*)(href|src|content)=['\"]?([^>'\"\s]*)(\.)?(jpeg|jpg|gif|png|svg|css|js|ico)([^>'\"\s]*)['\"\s]?([^>]*)>~im", function($m) use($util) {
+                    $src = $m[4].$m[5].$m[6].$m[7];
+                    $src = $util->replaceLink($src);
+                    return "<{$m[1]}{$m[2]}{$m[3]}=\"{$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src\"{$m[8]}>";
+                }, $this->pageHTML);
 
-            //proxy <style>: { }...url() ...}"
-            $this->pageHTML = preg_replace_callback("~<style([^>]*)?>(\s[^<>]*?\s*)?</style>~im", function($m) use($util) {
-                $style = empty($m[2]) ? '' : $m[2];
-                $add = empty($m[1]) ? '' : $m[1];
-                if (!empty($style)) {
-                    $style = preg_replace_callback("~url(\s)*\(([^>'\"\s]*)\.(jpeg|jpg|gif|png|svg|ico)\)~im", function($mi) use($util) {
-                        $src = $mi[2] . '.' . $mi[3];
-                        $src = $util->replaceLink($src);
-                        return "url(\"{$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src\")";
-                    }, $style);
-                }
-                return "<style$add>$style</style>";
-            }, $this->pageHTML);
+                //proxy style="... url() ..."
+                $this->pageHTML = preg_replace_callback("~<([^>]*)style=['\"]?([^>'\"]*)url\s*\(([^>'\"\s]*)\.(jpeg|jpg|gif|png|svg|ico)([^>'\"\s\)]*)\)([^>\"\']*)['\"]?([^>]*)>~im", function($m) use($util) {
+                    $src = $m[3].'.'.$m[4].$m[5];
+                    $src = $util->replaceLink($src);                
+                    return "<{$m[1]}style=\"{$m[2]}url({$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src){$m[6]}\"{$m[7]}>";
+                }, $this->pageHTML);
 
-            //proxy anchors
-            $this->pageHTML = preg_replace_callback("~<a(\s[^>]*?\s*)href=[\"']?([^>'\"\s]*)['\"\s]?([^>]*)>~i", function($m) use($util) {
-                $link = $m[2];
-                //ignore magnet links
-                if (preg_match('~^magnet:~', $link) == 1) {
-                    return $m[0];
-                }
-                $link = $util->replaceLink($link);
-                $replacement = $this->params['proxyDirParam'] . '/index.php?' . $this->params['proxyPageParam'] . '=' . $link;
-                return "<a{$m[1]}href=\"$replacement\"{$m[3]}>";
-            }, $this->pageHTML);
-            $this->echoPage();
+                //proxy <style>: { }...url() ...}"
+                $this->pageHTML = preg_replace_callback("~<style([^>]*)?>(\s[^<>]*?\s*)?</style>~im", function($m) use($util) {
+                    $style = empty($m[2]) ? '' : $m[2];
+                    $add = empty($m[1]) ? '' : $m[1];
+                    if (!empty($style)) {
+                        $style = preg_replace_callback("~url(\s)*\(([^>'\"\s]*)\.(jpeg|jpg|gif|png|svg|ico)\)~im", function($mi) use($util) {
+                            $src = $mi[2] . '.' . $mi[3];
+                            $src = $util->replaceLink($src);
+                            return "url(\"{$this->params['proxyDirParam']}/{$this->params['getPHP']}?u=$src\")";
+                        }, $style);
+                    }
+                    return "<style$add>$style</style>";
+                }, $this->pageHTML);
+
+                //proxy anchors
+                $this->pageHTML = preg_replace_callback("~<a(\s[^>]*?\s*)href=[\"']?([^>'\"\s]*)['\"\s]?([^>]*)>~i", function($m) use($util) {
+                    $link = $m[2];
+                    //ignore magnet links
+                    if (preg_match('~^magnet:~', $link) == 1) {
+                        return $m[0];
+                    }
+                    $link = $util->replaceLink($link);
+                    $replacement = $this->params['proxyDirParam'] . '/index.php?' . $this->params['proxyPageParam'] . '=' . $link;
+                    return "<a{$m[1]}href=\"$replacement\"{$m[3]}>";
+                }, $this->pageHTML);
+                $this->echoPage();
+            }
         } else {
             $this->echoDefault();
         }
